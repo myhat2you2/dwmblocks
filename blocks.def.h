9208d62662be39f0620cf061f0bb853a0d3062e8
myhat2you2 <joshuadtalbot3@gmail.com>
2022-09-03 18:27:07 +0100

    interval update

--- blocks.def.h
+++ blocks.def.h
@@ -5,16 +5,16 @@ static const Block blocks[] = {
 	 * {"",		"record",	0,		12},
 	 * {"",		"music",	0,		11},
 	 */
-	 {"",		"clock",	0,		10},
+	 {"",		"clock",	15,		10},
 	 //{"",		"volume",	0,		 9},
-	 {"",		"memory",	0,		 8},
-	 {"",		"cpu",		0,		 7},
-	 {"",		"disk /home",	0,		 6},
+	 {"",		"memory",	30,		 8},
+	 {"",		"cpu",		30,		 7},
+	 {"",		"disk /home",	30,		 6},
 	 //{"",		"mail",		0,		 5},
 	 //{"",		"updates",	0,		 4},
 	 //{"",		"torrent",	0,		 3},
-	 {"",		"internet",	0,		 2},
-	 {"",		"battery",	0,		 1},
+	 {"",		"internet",	10,		 2},
+	 {"",		"battery",	15,		 1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
