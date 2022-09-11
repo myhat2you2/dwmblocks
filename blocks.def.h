05584cdb63cc86d87ed8e59930ba2c483542a48b
myhat2you2 <joshuadtalbot3@gmail.com>
2022-09-11 03:46:46 +0100

    minor update

--- blocks.def.h
+++ blocks.def.h
@@ -6,7 +6,7 @@ static const Block blocks[] = {
 	 * {"",		"music",	0,		11},
 	 */
 	 {"",		"clock",	15,		10},
-	 //{"",		"volume",	0,		 9},
+	 {"",		"volume",	0,		 9},
 	 {"",		"memory",	30,		 8},
 	 {"",		"cpu",		30,		 7},
 	 {"",		"disk /home",	30,		 6},
@@ -14,7 +14,7 @@ static const Block blocks[] = {
 	 //{"",		"updates",	0,		 4},
 	 //{"",		"torrent",	0,		 3},
 	 {"",		"internet",	10,		 2},
-	 {"",		"battery",	15,		 1},
+	 {"",		"battery",	30,		 1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
